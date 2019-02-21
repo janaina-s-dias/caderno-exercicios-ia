d96a014d497bb7257e9ec3d0ca2c454c4129df4b
janainadias <janaina.dias@modalgr.com.br>
2019-02-21 15:56:28 -0300

    first commit

---
+++ src/exericiodois/ExercicioDois.java
@@ -0,0 +1,24 @@
+package exericiodois;
+
+public class ExercicioDois {
+
+	
+	/*
+	 * Crie uma classe Java e defina um método que compara duas cadeias de caracteres (A e B).
+		Utilize os critérios a seguir:
+			a. Se as cadeias de caracteres forem iguais, a função deverá retornar 1.0 (100%);
+			b. Se não forem, os aspectos apresentados na Tabela 2 devem ser considerados*/
+	
+	/*
+	 * Pontuação Característica
+		Soma um ponto Para cada caractere de A que constar na palavra B.
+		Soma dois pontos Para cada caractere de A que constar na mesma
+		proporção em B.
+		Soma três pontos Para cada caractere de A que constar na mesma posição em B.*/
+	
+	/*
+	 * ATENÇÃO: Observe que a pontuação NÃO deve ser o retorno do método, pois a função deve retornar 1.0 
+	 * se a palavra comparada atingir a pontuação máxima para todas as características.*/
+	
+	
+}
